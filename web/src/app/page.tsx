e495d1bec2c42497a9017b65c13229cc83dcbc58
Shan Ali <shanali0640@gmail.com>
2025-04-07 00:28:47 -0400

    fix: updated to dynamic players

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,66 @@
-import Image from "next/image";
+"use client"
+
+import Link from 'next/link'
+import React, { useState, useEffect } from 'react';
+
+export type Player = {
+  name: string;
+  description: string;
+};
 
 export default function Home() {
+  const [players, setPlayers] = useState<Player[]>([]);
+  const [playerName, setPlayerName] = useState('');
+
+  const backend = 'http://127.0.0.1:8000';
+
+  const addPlayer = async () => {
+    const newPlayer: Player =
+    {
+      name: playerName,
+      description: "loren ipsum"
+    };
+
+    const response = await fetch(`${backend}/addPlayer`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newPlayer),
+    });
+
+    if (response.ok) {
+      getPlayers();
+      setPlayerName('');
+    }
+  };
+
+  const getPlayers = async () => {
+    const response = await fetch(`${backend}/players`);
+    if (response.ok) {
+      const data = await response.json();
+      setPlayers(data);
+    }
+  };
+
+  useEffect(() => {
+    getPlayers();
+  }, []);
+
   return (
     <div>
+
+      <input type="text"
+        value={playerName}
+        onChange={(e) => setPlayerName(e.target.value)}
+        placeholder="Enter player name" />
+      <button onClick={addPlayer}>add Player</button>
+
+      <ul>
+        {players.map((player) => (
+          <li key={player.name}>
+            <Link href={`/player/${player.name}`}>{player.name}</Link>
+          </li>
+        ))}
+      </ul>
       nba stas
     </div>
   );
