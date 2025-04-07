2b0c0831aa30d47eb6fc3b9f0e87257b5d3a17f3
Shan Ali <shanali0640@gmail.com>
2025-04-07 00:28:22 -0400

    feat: added basic endpoints

--- api/app.py
+++ api/app.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import logging
 import os
+from typing import List
+from pydantic import BaseModel
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -21,9 +23,39 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+temp_db = []
+
+class Player(BaseModel):
+    name: str
+    description: str
+
+
 @app.get("/")
 async def get_projects():
     """ tesdt """
+    return None
+
+
+@app.post("/addPlayer")
+async def add_player(player: Player):
+    """ add a player """
+    player_dict = player.dict()
+    temp_db.append(player_dict)
+    print(f"{temp_db}") 
+    return player
+
+@app.get("/players")
+async def get_Players():
+    """ get all players from db """
+    return temp_db
+
+@app.get("/player/{player_name}", response_model=Player)
+async def get_player(player_name: str):
+    """ get data for player """
+    print(player_name)
+    for p in temp_db:
+        if p["name"] == player_name:
+            return p
     return None
 
 if __name__ == "__main__":
